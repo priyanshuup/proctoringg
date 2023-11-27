a7508777843e25814d200a77d821d6bfa4a4c727
priyanshuup <109269025+priyanshuup@users.noreply.github.com>
2023-11-27 11:26:46 +0530

    Update RunHuman.jsx

--- components/RunHuman.jsx
+++ components/RunHuman.jsx
@@ -97,10 +97,12 @@ class RunHuman extends Component {
 
     if (!this.video.paused) {
       const interpolated = this.human.next(this.human.result);
-      const { gesture: interpolatedGesture, object: interpolatedObject } = interpolated;
+      const { gesture: interpolatedGesture, object: interpolatedObject } =
+        interpolated;
       console.log("Interpolated Gesture:", interpolatedGesture);
       console.log("Interpolated Object:", interpolatedObject);
       this.human.draw.canvas(this.video, this.canvas);
+      // this.human.draw.all(this.canvas, interpolated);
 
       const checkGesture = (gesture, storageKey, warningThreshold) => {
         if (interpolatedGesture.some((item) => item.gesture === gesture)) {
@@ -109,53 +111,97 @@ class RunHuman extends Component {
           localStorage.setItem(storageKey, parseInt(storedCount) + 1);
           const currentCount = parseInt(storedCount) + 1;
           if (currentCount >= warningThreshold) {
-            this.setState((prevState) => ({ warningCount: prevState.warningCount + 1 }));
+            this.setState((prevState) => ({
+              warningCount: prevState.warningCount + 1,
+            }));
+            alert(
+              `Warning: You are ${gesture} too often! (Warning Count: ${
+                this.state.warningCount + 1
+              })`
+            );
             console.log(
-              `Warning: You are looking ${gesture} too often! (Warning Count: ${this.state.warningCount + 1})`
+              `Warning: You are looking ${gesture} too often! (Warning Count: ${
+                this.state.warningCount + 1
+              })`
             );
             localStorage.setItem(storageKey, 0);
           }
         }
       };
 
-      checkGesture("head down", "headDownCount", 30);
-      checkGesture("facing left", "headLeftCount", 50);
-      checkGesture("facing right", "headRightCount", 50);
-      checkGesture("head up", "headUpCount", 50);
+      checkGesture("head down" && "looking down", "headDownCount", 50);
+      checkGesture("facing left", "headLeftCount", 100);
+      checkGesture("facing right", "headRightCount", 100);
+      checkGesture("head up", "headUpCount", 100);
 
-      if (interpolatedObject.some((item) => item.label === "person" && item.score > 0.25)) {
-        const personCount = interpolatedObject.filter((item) => item.label === "person").length;
+      if (
+        interpolatedObject.some(
+          (item) => item.label === "person" && item.score > 0.25
+        )
+      ) {
+        const personCount = interpolatedObject.filter(
+          (item) => item.label === "person" && item.score > 0.3
+        ).length;
         if (personCount >= 2) {
-          this.setState((prevState) => ({ warningCount: prevState.warningCount + 1 }));
-          console.log("More than 1 person detected in the frame");
-          console.log(
-            `Warning: More than 1 person detected! (Warning Count: ${this.state.warningCount + 1})`
+          const storedUpCount =
+            localStorage.getItem("otherPersonDetected") || 0;
+          localStorage.setItem(
+            "otherPersonDetected",
+            parseInt(storedUpCount) + 1
           );
+          const currentUpCount = parseInt(storedUpCount) + 1;
+          const warningThreshold = 10;
+          if (currentUpCount >= warningThreshold) {
+            this.setState((prevState) => ({
+              warningCount: prevState.warningCount + 1,
+            }));
+            alert("More than 1 person Detected");
+            console.log(
+              `Warning: More than 1 Person Detected! (Warning Count: ${
+                this.state.warningCount + 1
+              })`
+            );
+            localStorage.setItem("otherPersonDetected", 0);
+          }
         }
-      } else if (interpolatedObject.some((item) => item.label !== "person")) {
+      } else if (interpolatedObject.length === 0 || interpolatedObject.some((item) => item.label !== "person")) {
+        // No person detected or an item with label other than "person"
         console.log("No Person Detected");
         const storedUpCount = localStorage.getItem("noPerson") || 0;
         localStorage.setItem("noPerson", parseInt(storedUpCount) + 1);
         const currentUpCount = parseInt(storedUpCount) + 1;
-        const warningThreshold = 30;
+        const warningThreshold = 10;
         if (currentUpCount >= warningThreshold) {
-          this.setState((prevState) => ({ warningCount: prevState.warningCount + 1 }));
+          this.setState((prevState) => ({
+            warningCount: prevState.warningCount + 1,
+          }));
           console.log(
             `Warning: No Person Detected! (Warning Count: ${this.state.warningCount + 1})`
           );
+          alert("No person Detected");
           localStorage.setItem("noPerson", 0);
         }
       }
+      
 
-      if (interpolatedObject.some((item) => item.label === "cell phone")) {
+      if (
+        interpolatedObject.some(
+          (item) => item.label === "cell phone" && item.score > 0.1
+        )
+      ) {
         const storedUpCount = localStorage.getItem("cellPhone") || 0;
         localStorage.setItem("cellPhone", parseInt(storedUpCount) + 1);
         const currentUpCount = parseInt(storedUpCount) + 1;
         const warningThreshold = 50;
         if (currentUpCount >= warningThreshold) {
-          this.setState((prevState) => ({ warningCount: prevState.warningCount + 1 }));
+          this.setState((prevState) => ({
+            warningCount: prevState.warningCount + 1,
+          }));
+          alert("Cell Phone Detected");
           console.log(
-            `Warning: Cell Phone Detected (Warning Count: ${this.state.warningCount + 1})`
+            `Warning: Cell Phone Detected (Warning Count: ${
+              this.state.warningCount + 1
+            })`
           );
           localStorage.setItem("cellPhone", 0);
         }
